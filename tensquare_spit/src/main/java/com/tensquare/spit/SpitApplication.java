d7ee9b45e9a5b6643c770939f4a9ab87565cb392
CrownClown <714491085@qq.com>
2019-11-04 20:35:03 +0800

    QM

--- tensquare_spit/src/main/java/com/tensquare/spit/SpitApplication.java
+++ tensquare_spit/src/main/java/com/tensquare/spit/SpitApplication.java
@@ -1,12 +1,13 @@
 package com.tensquare.spit;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import util.IdWorker;
-
+@SpringBootApplication
 public class SpitApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringApplication.class);
+        SpringApplication.run(SpitApplication.class);
     }
     @Bean
     public IdWorker idWorker(){
